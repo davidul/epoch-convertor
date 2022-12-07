07044062f1788ffacda9ecfae3259f72390fcb3a
David Ulicny <ulicny.david@gmail.com>
2022-12-07 06:18:55 +0100

    current time

--- cmd/nowCmd.go
+++ cmd/nowCmd.go
@@ -12,7 +12,7 @@ var nowCmd = &cobra.Command{
 	Long:  "now displays current time in unix epoch",
 	Run: func(cmd *cobra.Command, args []string) {
 		now := time.Now()
-		fmt.Printf("%s \n", now.Format("2006-01-02T15:04:05"))
+		fmt.Printf("Current time is: %s \n", now.Format("2006-01-02T15:04:05"))
 		fmt.Printf("Unix epoch seconds: %d \n", now.Unix())
 		fmt.Printf("Unix epoch miliseconds: %d \n", now.UnixMilli())
 	},
