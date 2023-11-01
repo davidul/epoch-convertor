34d5a468905e352f050ede984b2d50d65ad0622c
David Ulicny <ulicny.david@gmail.com>
2023-11-01 19:08:15 +0100

    fix test

--- cmd/fmtCmd_test.go
+++ cmd/fmtCmd_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 )
 
+var output = "Reference    01/01 12:00:00AM '19 +0000 \nANSIC    Tue Jan  1 00:00:00 2019 \nKitchen  12:00AM \nRFC822   01 Jan 19 00:00 UTC \nRFC822Z 01 Jan 19 00:00 +0000 \nRFC850   Tuesday, 01-Jan-19 00:00:00 UTC \nRFC1123  Tue, 01 Jan 2019 00:00:00 UTC \nRFC1123Z Tue, 01 Jan 2019 00:00:00 +0000 \nRFC3339  2019-01-01T00:00:00Z \nRFC3339Nano 2019-01-01T00:00:00Z \nUnixDate Tue Jan  1 00:00:00 UTC 2019 \nRubyDate Tue Jan 01 00:00:00 +0000 2019 \nStamp Jan  1 00:00:00 \nStamp Milli Jan  1 00:00:00.000 \nStamp Micro Jan  1 00:00:00.000000 \nStamp Nano Jan  1 00:00:00.000000000 \nDateOnly 2019-01-01 \nDateOnly 2019-01-01 00:00:00 \nTimeOnly 00:00:00 \n"
+
 func TestExecute_Fmt(t *testing.T) {
 	b := new(bytes.Buffer)
 	RootCmd.SetOut(b)
@@ -14,5 +16,5 @@ func TestExecute_Fmt(t *testing.T) {
 	RootCmd.SetArgs([]string{"fmt"})
 	SetStaticTime(&pkg.MockTime{})
 	RootCmd.Execute()
-	assert.Equal(t, "ANSIC    Tue Jan  1 00:00:00 2019 \nKitchen  12:00AM \nRFC822   01 Jan 19 00:00 UTC \nRFC850   Tuesday, 01-Jan-19 00:00:00 UTC \nRFC1123  Tue, 01 Jan 2019 00:00:00 UTC \nRFC3339  2019-01-01T00:00:00Z \nUnixDate Tue Jan  1 00:00:00 UTC 2019 \nRubyDate Tue Jan 01 00:00:00 +0000 2019 \n", b.String())
+	assert.Equal(t, output, b.String())
 }
